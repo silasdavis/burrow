80dddbf65dafd17ca9bdd2b8053164ce03e8c92e
Sean Young <sean@mess.org>
2021-02-03 14:02:14 +0000

    Remove gas hack now that solang has been patched
    https://github.com/hyperledger-labs/solang/pull/379
    
    Signed-off-by: Sean Young <sean@mess.org>

--- execution/wasm/contract.go
+++ execution/wasm/contract.go
@@ -81,9 +81,7 @@ func (ctx *context) ResolveFunc(module, field string) lifeExec.FunctionImport {
 	switch field {
 	case "call":
 		return func(vm *lifeExec.VirtualMachine) int64 {
-			// FIXME: gasLimit is being set to 0, solang issue or elsewhere?
-			//gasLimit := big.NewInt(vm.GetCurrentFrame().Locals[0])
-			gasLimit := big.NewInt(23020302030)
+			gasLimit := big.NewInt(vm.GetCurrentFrame().Locals[0])
 			addressPtr := uint32(vm.GetCurrentFrame().Locals[1])
 			valuePtr := int(uint32(vm.GetCurrentFrame().Locals[2]))
 			dataPtr := uint32(vm.GetCurrentFrame().Locals[3])
